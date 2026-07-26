89d656508c1d3e0de3f393096370ad701788fec2
PAUPAULUNA <luna.paulyne@gmail.com>
2026-07-26 21:59:38 +0800

    sidebar structured

--- frontend/src/components/shared/Sidebar.jsx
+++ frontend/src/components/shared/Sidebar.jsx
@@ -7,27 +7,44 @@ import Notification from '../../assets/bell.png';
 
 const Sidebar = () => {
   return (
+<div className="container">
+    
     <aside className="sidebar">
         {/* LOGO */}
         <div className="logo">
             <img src={logo} className="logo" alt="delicere logo" />
-            <h1 className="name">Delicere</h1>
+            <h2>Delicere</h2>
         </div>
-        {/* USER */}
+       
         <div>
-            <div>
-                <userCircle className="user" />
+            <ul className="sidebar-menu">
+                <li className="active">Dashboard</li>
+                <li>User Management</li>
+                <li>Sales Report</li>
+                <li>Inventory Control</li>
+                <li>System Settings</li>
+            </ul>
+        </div>
+         {/* USER */}
+        <div className="bottom-section">
+            <div className="user-info">
+                <img src={userCircle} className="user" alt="avatar" />
                 <div>
-                    <h1>John Doe</h1>
-                    <p>Waiter</p>
+                    <h1><u>Pau Luna</u></h1>
+                    <p>Admin</p>
                 </div>
             </div>
-            <div>
-                <Notification className="bell" />
+            <div className="Notification">
+                <img src={Notification} className="bell-icon" alt="notification" />
             </div>
+        </div>
+            <div className="logout-btn">
+                <img src={logout} className="logout" alt="logout" />
+                <p>Sign Out</p>
         </div>
 
     </aside>
+</div>
   )
 }
 
