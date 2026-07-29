3a2f97f120a74efb0618958d7697c3de47bfd472
PAUPAULUNA <luna.paulyne@gmail.com>
2026-07-29 20:55:52 +0800

    Left section done

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -4,6 +4,7 @@ import InfoCards from '../../components/MainInterface/InfoCards'
 import orderBell from '../../assets/orderBell.png'
 import money from '../../assets/money.png'
 import alarmclock from '../../assets/alarm-clock.png'
+import OrderList from '../../components/MainInterface/OrderList'
 
 const Dashboard = () => {
     
@@ -14,10 +15,14 @@ const Dashboard = () => {
                                 <div className="left-section">
                                         <Greetings />
                                         <div className="info-Cards">
-                                                <InfoCards title="Recent Orders" icon={<img src={orderBell} className="orderBell" alt="orderBell" />} number={16} footerNum={35}/>
+                                                <InfoCards title="Recent Orders" icon={<img src={orderBell} className="orderBell" alt="orderBell" />} number={2} footerNum={35}/>
                                                 <InfoCards title="Total Sales" icon={<img src={money} className="money" alt="money" />} number={50} footerNum={4}/>
-                                                <InfoCards title="Waiting List" icon={<img src={alarmclock} className="alarmclock" alt="alarmclock" />} number={7} footerNum={2}/>
+                                                <InfoCards title="Waiting List" icon={<img src={alarmclock} className="alarmclock" alt="alarmclock" />} number={3} footerNum={2}/>
                                         </div>
+                                        <div>
+                                                <OrderList />
+                                        </div>
+                                        
                                 </div>
                          {/* Right Section */}
                                 <div className="right-section">
